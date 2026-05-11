83589c86557fae153add2c8d3cdf265954d1b922
Michael2802 <maicolriola@hotmail.es>
2026-05-11 05:29:36 -0500

    fix: navbar responsive y configuracion vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
+export default defineConfig({
+  plugins: [react()],
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+
+  server: {
+    host: true,
+    port: 8080,
+  },
+});
